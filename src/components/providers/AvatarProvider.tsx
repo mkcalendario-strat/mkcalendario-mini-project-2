79e777b4fcf53099823481e46a983aa0210e4fa0
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-07-09 12:52:46 +0800

    feat: add avatar provider

---
+++ src/components/providers/AvatarProvider.tsx
@@ -0,0 +1,17 @@
+import ReactNiceAvatar, { genConfig } from "react-nice-avatar";
+
+interface AvatarProviderProps {
+  size: string;
+  seed: string;
+}
+
+export default function AvatarProvider({ size, seed }: AvatarProviderProps) {
+  const config = genConfig(seed);
+
+  return (
+    <ReactNiceAvatar
+      {...config}
+      className={`aspect-square ${size}`}
+    />
+  );
+}
