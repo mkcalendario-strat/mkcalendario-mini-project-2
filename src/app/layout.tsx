8b6d884056ee82fea4f8dddf1dd0bcfa72eedf5b
mkcalendario <i_mark.calendario@stratpoint.com>
2025-07-10 13:54:19 +0800

    feat: implement toasts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import "@/vendors/fontawesome/css/all.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <ToastContainer />
       </body>
     </html>
   );
