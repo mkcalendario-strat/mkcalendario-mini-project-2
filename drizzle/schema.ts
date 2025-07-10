6e809a3c959b338252dd5a9081e46dfdc07a5451
mkcalendario <i_mark.calendario@stratpoint.com>
2025-07-10 10:47:04 +0800

    feat: pull schema from neon

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,6 +1,25 @@
-import { pgTable } from "drizzle-orm/pg-core"
-import { sql } from "drizzle-orm"
+import { sql } from "drizzle-orm";
+import { bigint, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-
-
-
+export const blogs = pgTable("blogs", {
+  // You can use { mode: "bigint" } if numbers are exceeding js number limitations
+  id: bigint({ mode: "number" })
+    .primaryKey()
+    .generatedAlwaysAsIdentity({
+      name: "blogs_id_seq",
+      startWith: 1,
+      increment: 1,
+      minValue: 1,
+      maxValue: 9223372036854775807,
+      cache: 1
+    }),
+  key: uuid().notNull(),
+  title: text().notNull(),
+  description: text().notNull(),
+  content: text().notNull(),
+  userName: text("user_name").notNull(),
+  userAvatarSeed: text("user_avatar_seed").notNull(),
+  timestamp: timestamp({ mode: "string" })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull()
+});
