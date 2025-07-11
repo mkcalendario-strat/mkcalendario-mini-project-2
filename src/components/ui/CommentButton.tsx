fd560a558a516faca617090f04de98b69ed17e7b
mkcalendario <i_mark.calendario@stratpoint.com>
2025-07-11 11:07:55 +0800

    feat: complete CommentButton UI function

--- src/components/ui/CommentButton.tsx
+++ src/components/ui/CommentButton.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import useUserData from "@/hooks/useUserData";
+import { Fragment, useState } from "react";
+import AvatarProvider from "../providers/AvatarProvider";
 import Button from "./Button";
+import Input from "./Input";
+import Modal from "./Modal";
+import Textarea from "./Textarea";
 
 interface CommentButtonProps extends Pick<Blog, "id"> {
   className?: string;
@@ -8,14 +14,82 @@ interface CommentButtonProps extends Pick<Blog, "id"> {
 
 export default function CommentButton({ id, className }: CommentButtonProps) {
   void id;
+  const [isVisible, setIsModalVisible] = useState(false);
+
+  const toggleModal = () => setIsModalVisible((prev) => !prev);
 
   const baseClasses = "bg-blue-500 text-neutral-100";
   const classes = `${baseClasses} ${className}`.trim();
 
   return (
-    <Button className={classes}>
-      <i className="far fa-message" />
-      Comment
-    </Button>
+    <Fragment>
+      <Button
+        className={classes}
+        onClick={toggleModal}>
+        <i className="far fa-message" />
+        Comment
+      </Button>
+
+      <AddCommentModal
+        visible={isVisible}
+        toggle={toggleModal}
+      />
+    </Fragment>
+  );
+}
+
+interface AddCommentModalProps {
+  visible: boolean;
+  toggle: () => void;
+}
+
+function AddCommentModal({ visible, toggle }: AddCommentModalProps) {
+  const { userName, userAvatarSeed } = useUserData();
+
+  const [formData, setFormData] = useState({
+    comment: "",
+    desiredKey: ""
+  });
+
+  const handleInputChange = (
+    evt: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = evt.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <Modal
+      visible={visible}
+      toggle={toggle}
+      title="Add Comment"
+      className="flex flex-col gap-2">
+      <div className="flex flex-wrap items-center gap-3">
+        <AvatarProvider
+          seed={userAvatarSeed}
+          size="w-[35px]"
+        />
+        <p className="font-medium">{userName}</p>
+      </div>
+      <Textarea
+        id="comment"
+        name="comment"
+        placeholder="Comment"
+        value={formData.comment}
+        onChange={handleInputChange}
+      />
+      <Input
+        id="desired-key"
+        name="desiredKey"
+        placeholder="Desired Key"
+        value={formData.desiredKey}
+        onChange={handleInputChange}
+        tip="Keep this key to edit or delete comment."
+        type="password"
+      />
+      <Button className="self-baseline bg-neutral-900 text-neutral-100">
+        Submit Comment
+      </Button>
+    </Modal>
   );
 }
