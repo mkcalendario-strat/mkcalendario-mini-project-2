2dc15383373c51624ebc7428577ce29a347ac7c8
mkcalendario <i_mark.calendario@stratpoint.com>
2025-07-11 08:47:33 +0800

    chore: allow vercel blob to allowed domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: process.env.NEXT_PUBLIC_BLOB_PROTOCOL!,
+        hostname: process.env.NEXT_PUBLIC_BLOB_HOSTNAME!,
+        pathname: process.env.NEXT_PUBLIC_BLOB_PATHNAME!
+      }
+    ] as unknown as NonNullable<NextConfig["images"]>["remotePatterns"]
+  }
 };
 
 export default nextConfig;
