ae2f39df2017f90212bd4428599d750f369873a9
mkcalendario <i_mark.calendario@stratpoint.com>
2025-07-11 09:28:27 +0800

    feat: add modal component

---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+interface ModalProps {
+  title: string;
+  visible: boolean;
+  className?: string;
+  toggle: () => void;
+  children: React.ReactNode;
+}
+
+export default function Modal({
+  title,
+  toggle,
+  visible,
+  children,
+  className
+}: ModalProps) {
+  if (!visible) return null;
+
+  return (
+    <div className="fixed top-0 left-0 z-[1] h-full w-full bg-neutral-900/50">
+      <button
+        onClick={toggle}
+        className="fixed right-0 m-5 cursor-pointer">
+        <i className="far fa-times text-2xl text-white" />
+      </button>
+
+      <div className="fixed top-[50%] left-[50%] max-h-[80vh] w-[90%] translate-x-[-50%] translate-y-[-50%] overflow-y-scroll bg-white p-[20px] md:w-[600px]">
+        <div className="mb-7">
+          <p className="text-xl font-medium">{title}</p>
+        </div>
+
+        <div className={className}>{children}</div>
+      </div>
+    </div>
+  );
+}
