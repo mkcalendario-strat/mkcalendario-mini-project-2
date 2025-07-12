0d2b0661d60defccefec22d2d8f92d07427e3459
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-07-12 18:01:36 +0800

    feat: add Identity component

---
+++ src/components/providers/Identity.tsx
@@ -0,0 +1,33 @@
+"use server";
+
+import AvatarProvider from "./AvatarProvider";
+
+interface IdentityProps extends Identity {
+  reverse?: boolean;
+  imageSize: string;
+  description: string;
+}
+
+export default async function Identity({
+  reverse,
+  imageSize,
+  description,
+  userName,
+  userAvatarSeed
+}: IdentityProps) {
+  return (
+    <div className="flex flex-wrap items-center gap-3">
+      <div className="aspect-square">
+        <AvatarProvider
+          size={imageSize}
+          seed={userAvatarSeed}
+        />
+      </div>
+
+      <div className={`flex ${reverse ? "flex-col-reverse" : "flex-col"}`}>
+        <p className="leading-[18px] font-medium">{userName}</p>
+        <p className="text-sm leading-[18px] text-neutral-700">{description}</p>
+      </div>
+    </div>
+  );
+}
