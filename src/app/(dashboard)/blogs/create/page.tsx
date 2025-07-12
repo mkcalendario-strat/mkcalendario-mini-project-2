91b7ab98c7399bb8d663e181643988167dc3ae49
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-07-12 19:39:17 +0800

    feat: implement metadata on create blog

--- src/app/(dashboard)/blogs/create/page.tsx
+++ src/app/(dashboard)/blogs/create/page.tsx
@@ -1,6 +1,13 @@
 import CreateBlogForm from "@/components/sections/create-blog/CreateBlogForm";
 import { Fragment } from "react";
 
+export async function generateMetadata() {
+  return {
+    title: "Create Blog",
+    description: "Create blog that has sense."
+  };
+}
+
 export default function CreateBlog() {
   return (
     <Fragment>
