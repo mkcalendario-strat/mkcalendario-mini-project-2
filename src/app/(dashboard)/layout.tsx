3834fe5b70d752ea0d2cfcbb6ba1d92b062b6a2c
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-07-12 19:39:30 +0800

    feat: add metadata template

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,26 +1,20 @@
-"use client";
-
-import { initializeIdentity } from "@/actions/identity/identity";
 import Dashboard from "@/components/layouts/Dashboard";
-import { useCallback, useEffect, useState } from "react";
+
+export async function generateMetadata() {
+  return {
+    title: {
+      default: "Large",
+      template: "%s | Large"
+    },
+    description:
+      "A space for a sensible thoughts and ideas larger than any medium."
+  };
+}
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-  const [identity, setIdentity] = useState<boolean | null>(null);
-
-  const checkIdentity = useCallback(async () => {
-    await initializeIdentity();
-    setIdentity(true);
-  }, []);
-
-  useEffect(() => {
-    checkIdentity();
-  }, [checkIdentity]);
-
-  if (!identity) return null;
-
   return <Dashboard>{children}</Dashboard>;
 }
